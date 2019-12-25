bd333eede642c7f0498b0f16a728934b4cc6d908
Ilya Levin <ilya432@gmail.com>
2019-12-25 14:10:39 +0200

    add some stuff

--- App.jsx
+++ App.jsx
@@ -1,10 +1,20 @@
 import Home from './pages/Home.jsx'
+
+const Router = ReactRouterDOM.HashRouter
+const { Route, Switch } = ReactRouterDOM
+const { createBrowserHistory } = History
+const history = createBrowserHistory()
+
 class App extends React.Component {
 
     render() {
         return (
             <main>
-                <Home />
+                <Router history={history}>
+            <Switch>
+               <Route component={Home} path="/" exact></Route>
+            </Switch>
+         </Router>
             </main>
         )
     }

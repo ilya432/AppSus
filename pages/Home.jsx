445ed7255e008102ddcb680c090ae2c5302e96c7
Ilya Levin <ilya432@gmail.com>
2019-12-25 14:17:29 +0200

    sdfgwge

---
+++ pages/Home.jsx
@@ -0,0 +1,11 @@
+export default class Home extends React.Component {
+
+    render() {
+        return (
+            <section>
+                <h1>HOME</h1>
+                srwerwrgw
+            </section>
+        )
+    }
+}

b5f85813d5e8259de43d6cbda91c30103bc9dbad
Ilya Levin <ilya432@gmail.com>
2019-12-25 15:38:23 +0200

    another commit

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,10 +1,18 @@
 export default class Home extends React.Component {
+  render() {
+    return (
+      <section className="main-continer flex column">
+        <section>
+          {/* <h1>HOME</h1> */}
+          <nav className="nav flex row">
+            <div>
+              <span className="logo">Logo</span>
+            </div>
 
-    render() {
-        return (
-            <section>
-                <h1>HOME</h1>
-            </section>
-        )
-    }
+            <div className="nav-links">links list</div>
+          </nav>
+        </section>
+      </section>
+    );
+  }
 }

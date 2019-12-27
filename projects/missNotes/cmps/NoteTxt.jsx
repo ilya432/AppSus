3087e40791ed1c08dfff0f4a0ffb2105136010dc
Ilya Levin <ilya432@gmail.com>
2019-12-27 19:58:40 +0200

    new commit

--- projects/missNotes/cmps/NoteTxt.jsx
+++ projects/missNotes/cmps/NoteTxt.jsx
@@ -2,13 +2,8 @@ export default class NoteTxt extends React.Component {
   render() {
     return (
       <div className="n-note n-text-note flex column">
-
-        <div className="n-note-lable-container">
-          <span className="n-note-lable">{this.props.note.info.lable}</span>
-        </div>
-
         <div className="n-note-text-container">
-          <span className="n-note-txt">{this.props.note.info.txt}</span>
+          <span className="n-note-text">{this.props.note.info.txt}</span>
         </div>
 
       </div>
